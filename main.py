43e75ed803abd27569f5e0df4c4c86a9a1dc55f1
LuisArturo21 <arturo.gutierrez@iteso.mx>
2025-11-27 10:21:14 -0600

    Configurando despliegue automatico a EC2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = FastAPI(title="Notifications Worker")
-
+#test
 ENV = os.getenv("APP_ENVIRONMENT", "local")
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL") 
